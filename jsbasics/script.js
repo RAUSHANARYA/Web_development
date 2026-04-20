4d4e7c7fcb22352b04bd30f1a9e895b60b362b25
RAUSHANARYA <raushankumarlnct07@gmail.com>
2026-04-20 20:36:06 +0530

    JsOperator

---
+++ jsbasics/script.js
@@ -0,0 +1,32 @@
+/*
+js is a dyamic type language 
+
+var - var are use very less -
+    - we can use both same varibale 
+
+    no float ,doble in js == all number
+    var a = 5.4 ;
+
+    no char = all string 
+        var b = 'a';
+        var b = " hello";
+
+let - use for decalration
+    -let a=5 ; let b =6 ; 
+    -always variable name chnage 
+ 
+const 
+    - const pi = 3.2 = means fixed   
+
+*/
+
+
+
+console.log("hello world");
+console.log("hello world gandu ");
+
+let a = 10;
+let b = '10';
+
+console.log(a===b);
+
